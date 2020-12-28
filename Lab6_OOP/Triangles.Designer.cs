a0706f8a73f72c10200a1e061f2aa9a09c4f2d45
KaineKnight <66840596+KaineKnight@users.noreply.github.com>
2020-12-28 15:45:08 +0300

    Add files via upload

---
+++ Lab6_OOP/Triangles.Designer.cs
@@ -0,0 +1,399 @@
+﻿
+namespace Lab6_OOP
+{
+    partial class Triangles
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 38.25F);
+            this.label1.Location = new System.Drawing.Point(26, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(723, 59);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Равносторонний треугольник";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 38.25F);
+            this.label2.Location = new System.Drawing.Point(26, 195);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(741, 59);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Равнобедренный треугольник";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 38.25F);
+            this.label3.Location = new System.Drawing.Point(26, 439);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(712, 59);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Прямоугольный треугольник";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.button1.Location = new System.Drawing.Point(445, 121);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(166, 42);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Рассчитать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.button2.Location = new System.Drawing.Point(445, 301);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(166, 42);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Рассчитать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.button3.Location = new System.Drawing.Point(445, 563);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(166, 42);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Рассчитать";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 38.25F);
+            this.label4.Location = new System.Drawing.Point(26, 109);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(111, 59);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "a = ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 38.25F);
+            this.label5.Location = new System.Drawing.Point(26, 535);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(111, 59);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "a = ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 38.25F);
+            this.label6.Location = new System.Drawing.Point(26, 273);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(111, 59);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "a = ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 38.25F);
+            this.label7.Location = new System.Drawing.Point(26, 366);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(206, 59);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "angle = ";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 38.25F);
+            this.label8.Location = new System.Drawing.Point(26, 619);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(111, 59);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "b = ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 38.25F);
+            this.textBox1.Location = new System.Drawing.Point(126, 109);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(272, 65);
+            this.textBox1.TabIndex = 11;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 38.25F);
+            this.textBox2.Location = new System.Drawing.Point(126, 278);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(272, 65);
+            this.textBox2.TabIndex = 12;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 38.25F);
+            this.textBox3.Location = new System.Drawing.Point(220, 371);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(272, 65);
+            this.textBox3.TabIndex = 13;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 38.25F);
+            this.textBox4.Location = new System.Drawing.Point(126, 619);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(272, 65);
+            this.textBox4.TabIndex = 14;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 38.25F);
+            this.textBox5.Location = new System.Drawing.Point(126, 529);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(272, 65);
+            this.textBox5.TabIndex = 15;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 38.25F);
+            this.label9.Location = new System.Drawing.Point(632, 529);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(117, 59);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "S = ";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 38.25F);
+            this.label10.Location = new System.Drawing.Point(632, 278);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(117, 59);
+            this.label10.TabIndex = 17;
+            this.label10.Text = "S = ";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 38.25F);
+            this.label11.Location = new System.Drawing.Point(632, 68);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(117, 59);
+            this.label11.TabIndex = 18;
+            this.label11.Text = "S = ";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 38.25F);
+            this.label12.Location = new System.Drawing.Point(632, 136);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(117, 59);
+            this.label12.TabIndex = 19;
+            this.label12.Text = "P = ";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 38.25F);
+            this.label13.Location = new System.Drawing.Point(632, 366);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(117, 59);
+            this.label13.TabIndex = 20;
+            this.label13.Text = "P = ";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 38.25F);
+            this.label14.Location = new System.Drawing.Point(632, 619);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(117, 59);
+            this.label14.TabIndex = 21;
+            this.label14.Text = "P = ";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 38.25F);
+            this.textBox6.Location = new System.Drawing.Point(734, 68);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(302, 65);
+            this.textBox6.TabIndex = 22;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Font = new System.Drawing.Font("Microsoft Sans Serif", 38.25F);
+            this.textBox7.Location = new System.Drawing.Point(734, 619);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(302, 65);
+            this.textBox7.TabIndex = 23;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Font = new System.Drawing.Font("Microsoft Sans Serif", 38.25F);
+            this.textBox8.Location = new System.Drawing.Point(734, 523);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(302, 65);
+            this.textBox8.TabIndex = 24;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Font = new System.Drawing.Font("Microsoft Sans Serif", 38.25F);
+            this.textBox9.Location = new System.Drawing.Point(734, 371);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(302, 65);
+            this.textBox9.TabIndex = 25;
+            // 
+            // textBox10
+            // 
+            this.textBox10.Font = new System.Drawing.Font("Microsoft Sans Serif", 38.25F);
+            this.textBox10.Location = new System.Drawing.Point(734, 267);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(302, 65);
+            this.textBox10.TabIndex = 26;
+            // 
+            // textBox11
+            // 
+            this.textBox11.Font = new System.Drawing.Font("Microsoft Sans Serif", 38.25F);
+            this.textBox11.Location = new System.Drawing.Point(734, 139);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(302, 65);
+            this.textBox11.TabIndex = 27;
+            // 
+            // Triangles
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1077, 710);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Triangles";
+            this.Text = "Triangles";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox textBox11;
+    }
+}
---
+++ Lab6_OOP/Triangles.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TriangleClass;
+
+namespace Lab6_OOP
+{
+    public partial class Triangles : Form
+    {
+        string s1, s2, s2a, s3, s3b;
+        double a1, a2, angle2, a3, b3;
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            s3b = textBox4.Text;
+            if (s3b != "")
+            {
+                s3b = s3b.Replace('.', ',');
+                b3 = Convert.ToDouble(s3b);
+            }
+            else b3 = 0;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            t = new Right(a3, b3);
+            if (a3 > 0 && b3 > 0)
+            {
+                textBox8.Text = Convert.ToString(t.Area());
+                textBox7.Text = Convert.ToString(t.Perimeter());
+            }
+            else
+            {
+                textBox8.Text = "Нет данных";
+                textBox7.Text = "Нет данных";
+            }
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+            s3 = textBox5.Text;
+            if (s3 != "")
+            {
+                s3 = s3.Replace('.', ',');
+                a3 = Convert.ToDouble(s3);
+            }
+            else a3 = 0;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            t = new Isosceles(a2, angle2);
+            if (a2 > 0 && angle2 > 0 && angle2 < 180)
+            {
+                textBox10.Text = Convert.ToString(t.Area());
+                textBox9.Text = Convert.ToString(t.Perimeter());
+            }
+            else
+            {
+                textBox10.Text = "Нет данных";
+                textBox9.Text = "Нет данных";
+            }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            s2a = textBox3.Text;
+            if (s2a != "")
+            {
+                s2a = s2a.Replace('.', ',');
+                angle2 = Convert.ToDouble(s2a);
+            }
+            else angle2 = 0;
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            s2 = textBox2.Text;
+            if (s2 != "")
+            {
+                s2 = s2.Replace('.', ',');
+                a2 = Convert.ToDouble(s2);
+            }
+            else a2 = 0;
+        }
+
+        Triangle t = null;
+        public Triangles()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            t = new Equilateral(a1);
+            if (a1 > 0)
+            {
+                textBox6.Text = Convert.ToString(t.Area());
+                textBox11.Text = Convert.ToString(t.Perimeter());
+            }
+            else
+            {
+                textBox6.Text = "Нет данных";
+                textBox11.Text = "Нет данных";
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            s1 = textBox1.Text;
+            if (s1 != "")
+            {
+                s1 = s1.Replace('.', ',');
+                a1 = Convert.ToDouble(s1);
+            }
+            else a1 = 0;
+        }
+    }
+}
